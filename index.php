035e6ad36d6dba92ee6f19dab3bef59053577dfa
programingtaka <aiueooueno@gmail.com>
2022-11-29 00:10:56 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ブックサマリー</title>
+<link rel="stylesheet" href="./assets/css/style.css">
+</head>
+<body>
+    <header>
+        <nav id="gnav">
+            <ul class="inner">
+                
+            </ul>
+        </nav>
+    </header>
+
+    <!-- スレッドエリア-->
+    <div class="threadWrapper">
+        <div class="childWrapper">
+            <div class="threadTitle">
+                <span>[本のタイトル]</span>
+                <h1>anything</h1>
+            </div>
+            <section>
+                <article>
+                    <div class="wrapper">
+                        <div class="nameArea">
+                            <span>名前：</span>
+                            <p class="userName">Shincode</p>
+                            <time>2022/12/07</time>
+                        </div>
+                        <div class="summaryArea">
+                            <span>要約点：</span>
+                            <ol class="summary">
+                                <li>おもろー</li>
+                                <li>どうですか</li>
+                                <li>いい感じだった</li>
+                            </ol>
+                        </div>
+                        <div class="commentArea">
+                            <span>メモ：</span>
+                            <p class="comment">こめんとです</p>
+                        </div>
+                    </div>
+                </article>
+            </section>
+            <form class="formWrapper">
+                <div>
+                    <input type="submit" value="書き込む">
+                    <label>名前：</label>
+                    <input type="text">
+                </div>
+                <div>
+                    <textarea class="commentTextArea"></textarea>
+                </div>
+                
+            </form>
+        </div>
+    </div>
+
+    
+</body>
+</html>
